e088682949bd028f94b00e71c6b6ee473447c7dd
自己 <wudongdong@wus-macbook-pro.local>
2018-07-14 10:40:28 +0800

    百思不得姐

---
+++ Baisibudejie/Baisibudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baisibudejie
+//
+//  Created by 仵懂懂 on 2018/7/14.
+//  Copyright © 2018年 仵懂懂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baisibudejie/Baisibudejie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Baisibudejie
+//
+//  Created by 仵懂懂 on 2018/7/14.
+//  Copyright © 2018年 仵懂懂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
