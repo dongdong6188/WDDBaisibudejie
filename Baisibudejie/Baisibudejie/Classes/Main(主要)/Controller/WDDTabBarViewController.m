4c98ae75331963bc9864a7f38e65f3f7aac84014
自己 <wudongdong@wus-macbook-pro.local>
2018-07-15 11:46:53 +0800

    设置tabbar内容

--- Baisibudejie/Baisibudejie/Classes/Main(主要)/Controller/WDDTabBarViewController.m
+++ Baisibudejie/Baisibudejie/Classes/Main(主要)/Controller/WDDTabBarViewController.m
@@ -12,19 +12,43 @@
 #import "WDDNewViewController.h"
 #import "WDDPublishViewController.h"
 #import "WDDFriendTrendViewController.h"
+#import "UIImage+image.h"
 
 @interface WDDTabBarViewController ()
 
 @end
 
 @implementation WDDTabBarViewController
+//设置tabbar字体大小和颜色
++ (void)load{
+    UITabBarItem *item = [UITabBarItem appearanceWhenContainedIn:self, nil];
+    
+    NSMutableDictionary *attrsSelected = [NSMutableDictionary dictionary];
+    attrsSelected[NSForegroundColorAttributeName] = [UIColor blackColor];
+    [item setTitleTextAttributes:attrsSelected forState:UIControlStateSelected];
+    
+    NSMutableDictionary *attrsNormal = [NSMutableDictionary dictionary];
+    attrsNormal[NSFontAttributeName] = [UIFont systemFontOfSize:12];
+    [item setTitleTextAttributes:attrsNormal forState:UIControlStateNormal];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    [self addChildViewController];
+    //使用这种方法一样可以设置字体的颜色
+    //self.tabBar.tintColor=[UIColor blackColor];
+    //[self setupAllTabbarButtonItem];
+    [self addAllChildViewController];
 }
--(void)addChildViewController
+//设置子控制器按钮内容
+//-(void)setupAllTabbarButtonItem
+//{
+//    UINavigationController *nav1=self.childViewControllers[0];
+//    nav1.tabBarItem.title=@"精华";
+//    nav1.tabBarItem.image=[UIImage imageNamed:@"tabBar_essence_click_icon"];
+//
+//}
+// 添加所有子控制器
+-(void)addAllChildViewController
 {
     //创建导航控制器
     //精华
@@ -32,30 +56,47 @@
     vc1.view.backgroundColor=[UIColor redColor];
     UINavigationController *nav1=[[UINavigationController alloc]initWithRootViewController:vc1];
     [self addChildViewController:nav1];
-    //关注
-    WDDFriendTrendViewController *vc2=[[WDDFriendTrendViewController alloc]init];
-    vc2.view.backgroundColor=[UIColor blueColor];
+    //设置控制器内容
+    nav1.tabBarItem.title=@"精华";
+    nav1.tabBarItem.image=[UIImage imageNamed:@"tabBar_essence_icon"];
+    nav1.tabBarItem.selectedImage=[UIImage imageOriginalWithName:@"tabBar_essence_click_icon"];
+    //新帖
+    WDDNewViewController *vc2=[[WDDNewViewController alloc]init];
+    vc2.view.backgroundColor=[UIColor grayColor];
     UINavigationController *nav2=[[UINavigationController alloc]initWithRootViewController:vc2];
     [self addChildViewController:nav2];
+    
+    nav2.tabBarItem.title=@"新帖";
+    nav2.tabBarItem.image=[UIImage imageNamed:@"tabBar_new_icon"];
+    nav2.tabBarItem.selectedImage=[UIImage imageOriginalWithName:@"tabBar_new_click_icon"];
+    
+    //发布
+    WDDPublishViewController *vc3=[[WDDPublishViewController alloc]init];
+    vc3.view.backgroundColor=[UIColor yellowColor];
+    [self addChildViewController:vc3];
+    vc3.tabBarItem.image=[UIImage imageOriginalWithName:@"tabBar_publish_icon"];
+    vc3.tabBarItem.selectedImage=[UIImage imageOriginalWithName:@"tabBar_publish_click_icon"];
 
-    //新帖
-    WDDNewViewController *vc4=[[WDDNewViewController alloc]init];
-    vc4.view.backgroundColor=[UIColor grayColor];
+    
+    //关注
+    WDDFriendTrendViewController *vc4=[[WDDFriendTrendViewController alloc]init];
+    vc4.view.backgroundColor=[UIColor blueColor];
     UINavigationController *nav4=[[UINavigationController alloc]initWithRootViewController:vc4];
     [self addChildViewController:nav4];
     
-    //发布
-    WDDPublishViewController *vc5=[[WDDPublishViewController alloc]init];
-    vc5.view.backgroundColor=[UIColor yellowColor];
+    nav4.tabBarItem.title=@"关注";
+    nav4.tabBarItem.image=[UIImage imageNamed:@"tabBar_friendTrends_icon"];
+    nav4.tabBarItem.selectedImage=[UIImage imageOriginalWithName:@"tabBar_friendTrends_click_icon"];
+    
+    //我
+    WDDMeViewController *vc5=[[WDDMeViewController alloc]init];
+    vc5.view.backgroundColor=[UIColor orangeColor];
     UINavigationController *nav5=[[UINavigationController alloc]initWithRootViewController:vc5];
     [self addChildViewController:nav5];
     
-    //我
-    WDDMeViewController *vc3=[[WDDMeViewController alloc]init];
-    vc3.view.backgroundColor=[UIColor orangeColor];
-    UINavigationController *nav3=[[UINavigationController alloc]initWithRootViewController:vc3];
-    [self addChildViewController:nav3];
-
+    nav5.tabBarItem.title=@"我";
+    nav5.tabBarItem.image=[UIImage imageNamed:@"tabBar_me_icon"];
+    nav5.tabBarItem.selectedImage=[UIImage imageOriginalWithName:@"tabBar_me_click_icon"];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
